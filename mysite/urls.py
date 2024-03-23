8434eb64ed17230ae51c38625fd8213cbc7b6ae8
neallang <neal_langhorne@yahoo.com>
2024-03-23 14:58:39 -0400

    testing heroku link with base url

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from journalbuddy.views import journal
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', journal, name='journal')
 ]
