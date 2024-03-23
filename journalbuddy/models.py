38525ac067aee90722e3932059f714220ea759e3
neallang <neal_langhorne@yahoo.com>
2024-03-23 12:52:18 -0400

    userprofile model

--- journalbuddy/models.py
+++ journalbuddy/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    #one to one - each profile has one user, each user has one profile
+    #on delete cascade - when a userProfile is deleted, so is the user
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile') #user.profile
+    hobbies = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+    @staticmethod
+    def create_or_update_user_profile(user, **kwargs):
+        #if profile doesn't exist, create one. otherwise, update it.
+        profile, created = UserProfile.objects.get_or_create(user=user, defaults=kwargs)
+        #if profile already exists, update the info.
+        if not created:
+            for key, value in kwargs.items():
+                setattr(profile, key, value)
+            profile.save()
+
