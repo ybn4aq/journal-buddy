257b5ed5e339b37bd06a0d5eb8e371c40202cc68
helenryao <helenroseyao@gmail.com>
2024-03-23 13:15:47 -0400

    first changes

--- journalbuddy/views.py
+++ journalbuddy/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Journal
 
 # Create your views here.
+class JournalList(generic.ListView):
+    model = Journal
+    template_name = "journalList.html"
+    context_object_name = "journal_list"
+    
+    def get_queryset(self):
+        entries = Journal.objects.all()
+        return entries
