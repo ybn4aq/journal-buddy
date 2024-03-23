90697c549cc077714cd056c81fdc12b5a21a17b5
Thomas Arnold <ybn4aq@virginia.edu>
2024-03-23 16:13:48 -0400

    added calendar to urls.py

--- journalbuddy/urls.py
+++ journalbuddy/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('list/',views.JournalList.as_view(template_name='journalList.html'), name="list"),
+    path("calendar/", views.CalendarView.as_view(), name="calendar"),
 ]
