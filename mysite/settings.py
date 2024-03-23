01c8e3dbcbb5e118e98cb74f2a317e9a1d8d70f5
neallang <neal_langhorne@yahoo.com>
2024-03-23 15:50:12 -0400

    gunicorn in req.txt and updated link in allowed_hosts (set.py)

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    "https://journal-buddy-jb-06547030c822.herokuapp.com/",
+    'journal-buddy-jb-06547030c822.herokuapp.com/',
 ]
 
 
