9ea9dc11854ea131ea179cbe1b0e1f7b93646fdc
Thomas Arnold <ybn4aq@virginia.edu>
2024-03-24 11:48:01 -0400

    coewr

---
+++ mysite/config.py
@@ -0,0 +1,14 @@
+class Config:
+    def __init__(self):
+        self.databases = {
+            "default": {
+                "ENGINE": "django.db.backends.postgresql",
+                "NAME": "defgloaou24sef",
+                "USER": "ukrtpegeambrsu",
+                "HOST": "ec2-3-230-24-12.compute-1.amazonaws.com",
+                "PORT": "5432",
+                "PASSWORD": "1ab6a666caa011ac35d2ba0af905641bdd745003c0be2d1157ddf19b43c4de1a",
+                "URI": "postgres://ukrtpegeambrsu:1ab6a666caa011ac35d2ba0af905641bdd745003c0be2d1157ddf19b43c4de1a@ec2-3-230-24-12.compute-1.amazonaws.com:5432/defgloaou24sef",
+            }
+        }
+        self.secret = "django-insecure-4h#*ugv$=z%*6sh$b7n#1@mkt)%*b19lbeg2tlhbwj6ass%ork"
