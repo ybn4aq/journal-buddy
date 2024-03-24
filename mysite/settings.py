74ecb25b0b4b53dd3c4e5a75d8f8a02b2f9e9a8f
neallang <neal_langhorne@yahoo.com>
2024-03-24 09:30:45 -0400

    WORKING database credentials

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,12 +84,12 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "d1m413ssoam1vt",
-        "USER": "zwutrztzqanbjc",
-        "HOST": "ec2-52-54-200-216.compute-1.amazonaws.com",
+        "NAME": "defgloaou24sef",
+        "USER": "ukrtpegeambrsu",
+        "HOST": "ec2-3-230-24-12.compute-1.amazonaws.com",
         "PORT": "5432",
-        "PASSWORD": "3e68efe1eaf8e3329c3977a458d4d9f29aa73ef9b8d962c5cc7b653c82733399",
-        "URI": "postgres://qzxpamhukebrsm:1a20504f06f107c3090b23426dcc79ff9ea558aa4eaeb2936ce8825fba315b05@ec2-3-230-24-12.compute-1.amazonaws.com:5432/defgloaou24sef",
+        "PASSWORD": "1ab6a666caa011ac35d2ba0af905641bdd745003c0be2d1157ddf19b43c4de1a",
+        "URI": "postgres://ukrtpegeambrsu:1ab6a666caa011ac35d2ba0af905641bdd745003c0be2d1157ddf19b43c4de1a@ec2-3-230-24-12.compute-1.amazonaws.com:5432/defgloaou24sef",
     }
 }
 
