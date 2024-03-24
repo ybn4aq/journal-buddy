2a7c8988d60cfc14199bb2b0217efbfe394defa9
Thomas Arnold <ybn4aq@virginia.edu>
2024-03-23 20:17:08 -0400

    progress

--- journalbuddy/urls.py
+++ journalbuddy/urls.py
@@ -6,8 +6,7 @@ urlpatterns = [
     path('list/',views.JournalList.as_view(template_name='journalList.html'), name="list"),
     path("calendar/", views.CalendarView.as_view(), name="calendar"),
     path('journal/<str:username>/<int:year>/<int:month>/<int:day>/', views.JournalList.journal_for_user_and_day, name='journal_for_day'),
-    path("/", views.HomeView.as_view(), name="home"),
-    path("", include("journalbuddy.urls")),  # TODO: may be not necessary
+    path("", views.HomeView.as_view(), name="home"),
     path("login/", views.user_login, name="login"),
     path("signup/", views.user_signup, name="signup"),
     path("logout/", views.user_logout, name="logout")
