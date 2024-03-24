0b84c15611e7994e6e8f6b3daf499873824f2749
Thomas Arnold <ybn4aq@virginia.edu>
2024-03-23 20:52:53 -0400

    syntax errrors

--- journalbuddy/forms.py
+++ journalbuddy/forms.py
@@ -19,4 +19,4 @@ class SignupForm(UserCreationForm):
 
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput
+    password = forms.CharField(widget=forms.PasswordInput)
